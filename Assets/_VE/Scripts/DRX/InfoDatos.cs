5ea0f031dc179898571eb276b4c7dcf6ad88c7f3
Firesnow <Miguelpela222@gmail.com>
2024-09-26 13:27:08 -0500

    Actualización 26 de septiembre

--- Assets/_VE/Scripts/DRX/InfoDatos.cs
+++ Assets/_VE/Scripts/DRX/InfoDatos.cs
@@ -9,13 +9,25 @@ public class InfoDatos : MonoBehaviour
 {
     public ScriptableMolecula SMolecula;
 
-    
+    public float[] datosGrafica;
+    public string[] infoGrafica;
+
     public Text InfoNombre;
     public Text InfoAtomo;
 
     void Start()
     {
         Instantiate(SMolecula.atomoPrefab, Vector3.zero, Quaternion.identity,this.transform);
+        string InfoNombre = SMolecula.nombreAtomo;
+
+        for (int i = 0; i < SMolecula.graficas.Length; i++)
+        {
+            datosGrafica[i] = SMolecula.graficas[i];
+        }
+        for (int i = 0; i < SMolecula.graficas.Length; i++)
+        {
+            infoGrafica[i] = SMolecula.textoGraficas[i];
+        }
     }
 
 }
--- Assets/_VE/Scripts/DRX/ScriptableMolecula.cs
+++ Assets/_VE/Scripts/DRX/ScriptableMolecula.cs
@@ -10,10 +10,8 @@ public class ScriptableMolecula : ScriptableObject
     public GameObject atomoPrefab; // El GameObject que representará el átomo
     public string nombreAtomo; // El nombre del átomo
     [TextArea(minLines: 1, maxLines: 5)]
-    public string InfoAtomo; // Informacion basica sobre el atomo (fecha de i
+    public string infoAtomo; // Informacion basica sobre el atomo (fecha de i
     public float[] graficas; // La Informacion delas graficas 
-
-    
+    public string[] textoGraficas; // La Informacion delas graficas 
 
 }
-
